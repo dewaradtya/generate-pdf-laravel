21584935fffc2949184ff27bdbd53234b8b641f9
05 Dewa Raditya Rochman <dewarochman26@gmail.com>
2024-07-01 10:30:34 +0700

    feat: first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>tes</title>
+</head>
+<body>
+    <a href="{{ route('generate-pdf') }}">Generate PDF</a>
+    <p>This PDF document is generated using domPDF in Laravel.</p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PDFController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/generate-pdf', [PDFController::class, 'generatePDF'])->name('generate-pdf');
+Route::get('/home', [PageController::class, 'home']);
